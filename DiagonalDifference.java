1941bf191cb33e48020036ddd8d717b150a09244
Abhishek <kumarabhishek0388@gmail.com>
2019-03-05 17:50:28 +0530

    removing comments

--- DiagonalDifference.java
+++ DiagonalDifference.java
@@ -9,16 +9,10 @@ public class DiagonalDifference {
 		int diff = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
-				//System.out.println("in : "+i);
 				sumLTR = sumLTR +arr[i++][j];
-				//System.out.println(arr[i++][j]);
-				
 			}
-
 			for(int k = 0;k<arr.length;k++) {
 				sumRTL = sumRTL + arr[--i][k];
-				//System.out.println(arr[--i][k]);
-				
 			}
 			System.out.println("Sum RTL = "+ sumRTL);
 			System.out.println("Sum LTR = "+ sumLTR);
@@ -34,10 +28,9 @@ public class DiagonalDifference {
 	public static void main(String[] args) {
 
 		int[][] arr = new int[][] { { 11, 2, 4 },
-									{ 4, 5, 6 },
-									{ 10, 8, -12 }};
+					     { 4, 5, 6 },
+					    { 10, 8, -12 }};
 									
-		//System.out.println(arr.length);
 
 		int result = diagonalDifference(arr);
 		System.out.println(result);
