fda3d79eab9a6c58a8a84285c9a7704104d6bb3c
Abhishek <kumarabhishek0388@gmail.com>
2019-03-15 19:21:05 +0530

    typo correction

--- searching/binarysearch/BinarySearchAlgo.java
+++ searching/binarysearch/BinarySearchAlgo.java
@@ -11,7 +11,7 @@ public class BinarySearchAlgo {
 	 * @return index of value to be searched, returns -1 if the value is not in
 	 *         the array
 	 */
-	public static int binarySeach(int[] input, int key) {
+	public static int binarySearch(int[] input, int key) {
 
 		int low = input[0];
 		int high = input[input.length - 1];
@@ -30,7 +30,7 @@ public class BinarySearchAlgo {
 
 	public static void main(String args[]) {
 		int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		System.out.println(binarySeach(input, 4));
+		System.out.println(binarySearch(input, 4));
 	}
 
 }
