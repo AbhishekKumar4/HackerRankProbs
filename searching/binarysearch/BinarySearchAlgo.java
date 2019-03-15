269b81dd85e26b165cba30c3db99f5255ed7ea2f
KUMAR Abhishek <kumarabhishek0388@gmail.com>
2019-03-15 18:48:42 +0530

    adding binary search algo

---
+++ searching/binarysearch/BinarySearchAlgo.java
@@ -0,0 +1,39 @@
+package com.abhishek.hackerrank.search.binary;
+
+public class BinarySearchAlgo {
+
+	/**
+	 * 
+	 * @param input
+	 *            array
+	 * @param value
+	 *            to be searched
+	 * @return index of value to be searched, returns -1 if the value is not in
+	 *         the array
+	 */
+	public static int binarySeach(int[] input, int key) {
+
+		int low = input[0];
+		int high = input[input.length - 1];
+		while (low <= high) {
+			int mid = low + high / 2;
+			if (input[mid] < key) {
+				low = mid + 1;
+			} else if (input[mid] > key) {
+				high = mid - 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+
+	public static void main(String args[]) {
+		int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		long start = System.currentTimeMillis();
+		System.out.println(binarySeach(input, 4));
+		long end = System.currentTimeMillis();
+		System.out.println(end - start + "ms");
+	}
+
+}
